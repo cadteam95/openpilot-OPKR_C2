b2307fcd9661f1f865e12cecbd64a1df683dbc76
cadteam95 <cadteam95@gmail.com>
2026-05-19 20:25:41 +0900

    Revert Mappy to 3.4.11 and remove offline check
    최신 맵피 3.4.18 버전에서 오프라인 실행 시 네트워크 에러가 발생하여 앱이 비정상 종료되는 현상을 해결하기 위해 오프라인 기동이 완벽하게 지원되는 구버전 3.4.11 버전으로 롤백합니다. ui.cc에서 네트워크 유무를 체크해 기동을 가로막던 getConnectName 코드를 제거하여 오프라인에서도 즉시 자동 실행되도록 하였습니다. 맵피 미기동 시 맵핵이 오작동하여 블루스크린 팝업이 뜨지 않도록 map_is_running 플래그 체크를 보완하였습니다.

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -534,7 +534,7 @@ static void update_status(UIState *s) {
 
   //opkr navi on boot
   if (!s->scene.navi_on_boot && (s->sm->frame - s->scene.started_frame > 5*UI_FREQ)) {
-    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0) && s->scene.deviceState.getConnectName() != "---") {
+    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0)) {
       s->scene.navi_on_boot = true;
       s->scene.map_is_running = true;
       s->scene.map_on_top = true;
@@ -552,7 +552,7 @@ static void update_status(UIState *s) {
     }
   }
   if (!s->scene.move_to_background && (s->sm->frame - s->scene.started_frame > 20*UI_FREQ)) {
-    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("OpkrMapEnable") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0)) {
+    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("OpkrMapEnable") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0) && s->scene.map_is_running) {
       s->scene.move_to_background = true;
       s->scene.map_on_top = false;
       s->scene.map_on_overlay = true;
